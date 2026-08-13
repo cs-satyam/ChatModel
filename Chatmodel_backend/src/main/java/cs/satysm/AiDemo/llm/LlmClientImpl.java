ccc1214683023e548e8a7dee4c7c143b757c11cc
cs-satyam <satyamwork4464@gmail.com>
2026-08-13 10:16:26 +0530

    Fix formatting in AiModelConfig for consistency

--- Chatmodel_backend/src/main/java/cs/satysm/AiDemo/llm/LlmClientImpl.java
+++ Chatmodel_backend/src/main/java/cs/satysm/AiDemo/llm/LlmClientImpl.java
@@ -2,25 +2,21 @@ package cs.satysm.AiDemo.llm;
 
 import dev.langchain4j.model.chat.ChatModel;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 
 public class LlmClientImpl implements Llmclient {
 
+    @Qualifier("gemini")
     private final ChatModel geminiChatModel;
+    @Qualifier("ollama")
     private final ChatModel ollamaChatModel;
+    @Qualifier("grok")
     private final ChatModel grokChatModel;
-
-    public LlmClientImpl(
-            @Qualifier("gemini") ChatModel geminiChatModel,
-            @Qualifier("ollama") ChatModel ollamaChatModel,
-            @Qualifier("grok")ChatModel grokChatModel   ) {
-        this.geminiChatModel=geminiChatModel;
-        this.ollamaChatModel=ollamaChatModel;
-        this.grokChatModel=grokChatModel;
-    }
 
 
     @Override
